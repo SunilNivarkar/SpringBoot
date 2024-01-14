b2fff3ebb52c2dc2483b51fc0a674d8656140c73
Dev <dev@gmail.com>
2024-01-14 23:37:22 +0530

    wish.jsp commit

--- SpringBootMVCProj02-WishMessageApp/src/main/java/com/nt/controller/WishMessageGenerator.java
+++ SpringBootMVCProj02-WishMessageApp/src/main/java/com/nt/controller/WishMessageGenerator.java
@@ -2,6 +2,7 @@ package com.nt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,20 +16,28 @@ public class WishMessageGenerator {
 	public String showMessage() {
 		return "home";  //LVN
 	}
+	/*	@RequestMapping("/wish")
+		public ModelAndView generateMessage() {
+			//use service
+			String result=service.generateWishMessage();
+			//return MAV
+			ModelAndView mav=new ModelAndView();
+			mav.addObject("wMsg",result); //Like this we can any number of attribute
+			mav.setViewName("display");
+			return mav;
+			//in this constructor first is LVN ,ModelAttr name MAvalue
+			//return new ModelAndView("display","wMsg",result); //Here we can only one model attribute
+			
+			
+			
+		}*/
+	
 	@RequestMapping("/wish")
-	public ModelAndView generateMessage() {
+	public void generateMessage(Model model) {
+		System.out.println("Shared Memory object class name:"+model.getClass());
 		//use service
 		String result=service.generateWishMessage();
-		//return MAV
-		ModelAndView mav=new ModelAndView();
-		mav.addObject("wMsg",result); //Like this we can any number of attribute
-		mav.setViewName("display");
-		return mav;
-		//in this constructor first is LVN ,ModelAttr name MAvalue
-		//return new ModelAndView("display","wMsg",result); //Here we can only one model attribute
-		
-		
-		
+		model.addAttribute("wMsg", result);
 	}
 
 }
